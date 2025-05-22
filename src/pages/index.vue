6ac639ccd7c356fcf7c3292ac6f71c6d03de75e6
Paulo Reis <pauloreisprofissional@gmail.com>
2025-05-22 19:27:29 -0300

    Select

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,36 @@
 <template>
-  <HelloWorld />
+  <v-layout>
+    <v-container>
+
+      <h1 class="mb-5">Bem-vindo à FIPE UNIME</h1>
+
+      <v-select
+        item-title="text"
+        item-value="value"
+        :items="items"
+        label="Item 2"
+      />
+
+    </v-container>
+  </v-layout>
 </template>
 
-<script setup>
-  //
+<script>
+
+  import { ref } from 'vue';
+
+  export default {
+    setup () {
+      const items = ref([
+        { text: 'Item 1', value: 1 },
+        { text: 'Item 2', value: 2 },
+        { text: 'Item 3', value: 3 },
+      ]);
+
+      return {
+        items,
+      };
+    },
+  };
+
 </script>
